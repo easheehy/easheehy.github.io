0025b679403508105768580d083daabcacdf6c5d
easheehy <easheehy@uci.edu>
2017-11-13 13:51:21 -0800

    update

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 var cur = -1;
 var max = 0;
-var profiles = [];
+var images = [];
 var interval = 1500;
 
 function start() {
-    profiles = document.profiles;
-    max = profiles.length;
+    images = document.images;
+    max = images.length;
     cur = 0;
 };
 
@@ -14,12 +14,12 @@ function rotate() {
 
 	cur = (cur >= max - 1) ? 0 : cur += 1;
     for (i = 0; i < max; i += 1) {
-		profiles[i].hidden = (i !== cur);
+		images[i].hidden = (i !== cur);
     }
 };
 
 window.onload = function() {
-    start();
-profiles[cur].hidden = false;
+start();
+images[cur].hidden = false;
 setInterval(rotate, interval);
 };
