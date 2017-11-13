9407569e8588b490e34a8e6694b5d63592ff409f
easheehy <easheehy@uci.edu>
2017-11-13 12:58:09 -0800

    test

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
+var index = 0;
 
+function changeBanner() {
+    [].forEach.call(document.images, function(v, i) {
+        document.images[i].hidden = i !== index
+    });
+    index = (index + 1) % document.images.length;
+}
+window.onload = function() {
+    setInterval(changeBanner, 100)
+};
 
 /*$("nav li").mouseenter(function(){
      $(this).css("color", "rgb(0,61,153)");
